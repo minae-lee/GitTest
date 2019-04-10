78a1542afeaf0e9d9fe5244ee24c62ef55f03f73
minaelee <leeminae91@gmail.com>
2019-04-10 17:09:55 +0900

    eclipse에서 git 사용방법

---
+++ ectest.c
@@ -0,0 +1,14 @@
+eclipse에서의 git 사용법
+
+1. git 설치
+2. eclipse 설치
+3. git clone
+	1. eclipse 화면에서 'Checkout project from git' 선택
+	2. clone URL > Next
+	3. URL 입력, Authentication 입력 > Next
+	4. branch 선택 > Next
+	5. Directory 선택 > Next
+	6. Import as general project > Finish
+4. Open Perspective > Git > Open
+
+	
