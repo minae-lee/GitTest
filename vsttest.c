092a1cb2307abef7eec25b3e6e399d37b602aa1b
minaelee <leeminae91@gmail.com>
2019-04-10 16:17:27 +0900

    vst에서 git 사용법 작성
    Signed-off-by: minaelee <leeminae91@gmail.com>

--- vsttest.c
+++ vsttest.c
@@ -1,1 +1,11 @@
-Visual Studio Code에서의 git 사용 확인 - 1
+Visual Studio Code에서의 git 사용법
+
+1. git 설치
+2. visual studio code 설치
+3. git clone
+    1. vst에서 ctrl+shift+p 나 설정>Command Palet 
+    2. git clone 입력 후 repository 주소 입력
+    3. git bash에서 사용자 등록
+     - git config --global user.email "email"
+     - git config --global user.name "name"
+4. check버튼이 commit ... 에서 push
