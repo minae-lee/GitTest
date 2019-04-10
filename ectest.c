99f1e193fe64f177d36d0c1094a2a910aa81fe9a
minaelee <leeminae91@gmail.com>
2019-04-10 17:15:06 +0900

    jdk 설치 추가

--- ectest.c
+++ ectest.c
@@ -1,7 +1,7 @@
 eclipse에서의 git 사용법
 
 1. git 설치
-2. eclipse 설치
+2. jdk, eclipse 설치
 3. git clone
 	1. eclipse 화면에서 'Checkout project from git' 선택
 	2. clone URL > Next
@@ -10,5 +10,5 @@ eclipse에서의 git 사용법
 	5. Directory 선택 > Next
 	6. Import as general project > Finish
 4. Open Perspective > Git > Open
-
+5. 파일 수정 후 프로젝트 이름 > commit > commit message 입력 후 commit and push
 	
